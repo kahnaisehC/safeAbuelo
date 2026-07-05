4e99c92f9916c4363cac4c4c99f61ba8d19b44ca
JoaquinValdez <168864095+JoaquinValdez2302@users.noreply.github.com>
2026-07-05 12:10:45 -0300

    add pagination hook

---
+++ hooks/usePagination.ts
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+export function usePagination<T>(url: string) {
+  const [data, setData] = useState<T[]>([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      setLoading(true);
+
+      try {
+
+        const res = await fetch(`${url}?page=${page}`);
+        const json = await res.json();
+
+        setData(prev => {
+          for(let v of json){
+            prev.push(v)
+          }
+          return prev
+        });
+
+        setLoading(false);
+      }
+      catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      }
+
+    }
+
+    fetchData();
+  }, [url, page]);
+
+  return {
+    data,
+    loading,
+    page,
+    next: () => setPage((p) => p + 1),
+    setPage,
+  };
+}
